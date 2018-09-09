0fab3658bf102b68397e730bc3cdbe6cf29a21f3
Samantha Persad <samanthapersad14@gmail.com>
2018-09-09 19:54:35 -0400

    installed react-router-dom

--- little_angels_playhouse_client/src/App.js
+++ little_angels_playhouse_client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
 import Student from './components/Student';
 import Login from './components/Login';
 import Header from './components/Header';
@@ -146,12 +147,12 @@ class App extends Component {
   delete(id) {
     const jwt = localStorage.getItem("jwt")
     const init = { 
-      headers: {"Authorization": `Bearer ${jwt}`},
+      // headers: {"Authorization": `Bearer ${jwt}`},
       method: 'DELETE',
       mode: 'cors',
     }
     fetch(`${BASE_URL}/students/${id}`, init)
-    .then(()=> this.getJuices())
+    .then(()=> this.getStudents())
     .catch(err => err.message)
   }
 
@@ -220,6 +221,7 @@ class App extends Component {
                  />
     return (
       <div className="App">
+      <Navbar/>
         {/* <Header 
         logout={this.logout} 
         create={this.create}
--- little_angels_playhouse_client/src/components/Navbar.jsx
+++ little_angels_playhouse_client/src/components/Navbar.jsx
@@ -1,74 +1,19 @@
-const Header = (props) => {
+import React from 'react';
+
+const Navbar = (props) => {
     return (
 
-        //         body{
-        //             margin:0;
-        
-        //         }
-        //         ul li{
-        //             display:inline;
-        //             list-style:none;
-        //         }
-        //         ul{
-        //             background: #0072BB;
-        //         }
-        //         li a{
-        //             display: inline-block;
-        //             color: white;
-        //             text-align: center;
-        //             padding: 14px 16px;
-        //             text-decoration: none;
-        //         }
-        //         li a:hover, .dropdown:hover{
-        //              background-color:#2F52E0; 
-        //         }
-        //         .dropdown{
-        //             display: inline-block;
-        //         }
-        //         .content{
-        //              display: none;
-        //              position: absolute;
-        //              background-color:#f9f9f9;
-        //              min-width: 160px;
-        //              box-shadow: 0 8px 16px 0 rgba(0,0,0,0,2);
-        //         }
-        //         .content a{
-        //             color: black;
-        //             padding: 12px 16px;
-        //             text-decoration: none;
-        //             display: block;
-        //             text-align: left;
-        
-        //         }
-        //         .content a:hover{
-        //             background-color: #f1f1f1;
-        
-        //         }
-        //         .dropdown:hover .content{
-        //             display: block;
-        //         }
-        //     </style>
-        // </head>
-        // <body>
-        //     <ul>
-        //         <li><a href="#" class="active">Home</a></li>
-        //         <li class="dropdown"><a href="dropbtn" href="#">Drop</a></li>
-        //          <div class="content">
-        //              <a href="#">About</a>
-        //              <a href="#">Experience</a>
-        //              <a href="#">Contact</a>
-        
-        //          </div>
-        //          <li><a href="#"></a>Next</li>
-        //     </ul>
-        
-        
-        
-        // </body>
-      
+       <ul>
+<li><a href="#" class="active">Home</a></li>
+<li class="dropdown"><a href="dropbtn" href="#">Drop</a></li>
+<div class="content"> </div>
+   <a href="#">About</a>
+   <a href="#">Experience</a>
+   <a href="#">Contact</a>
 
 
-
+<li><a href="#"></a>Next</li>
+</ul>
     )
 }
 
