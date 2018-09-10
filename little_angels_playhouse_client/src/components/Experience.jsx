33c0ebc5e13eaf3b4f7cddc7b1d657099033e557
Samantha Persad <samanthapersad14@gmail.com>
2018-09-09 23:56:09 -0400

    home

--- little_angels_playhouse_client/src/components/Experience.jsx
+++ little_angels_playhouse_client/src/components/Experience.jsx
@@ -4,7 +4,21 @@ const Experience = (props) => {
     
     return (
         <div>
-            <h1>jhhggv</h1>
+            <h3>Little Angels Learning Experience</h3>
+            <p>
+            L.A.L.E.® Curriculum
+The Learning Experience's proprietary L.E.A.P. Curriculum was developed over 30 years with your child's optimal development in mind. Today, our Learning Experience Academic Program includes sign language for infants and toddlers, a phonics program, foreign language programs, manners and etiquette, physical fitness, and a philanthropy program that teaches children the value of selflessness and giving. All programs and lessons at TLE® are designed to make learning fun! And nothing is more fun than our L.E.A.P. Interactive boards that use touch-screen technology and interactive learning games to further enhance and engage children in our L.E.A.P. Curriculum programs.
+
+These programs, along with enrichment programs that are offered* at no additional charge, provide a well-rounded early education that has proven to get children on the path to a bright future.
+
+We believe that how children learn is as important as what they learn. With that idea in mind, the curriculum research and development team at TLE® created our proprietary L.E.A.P. (Learning Experience Academic Program) curriculum. This L.E.A.P. curriculum encourages each child to achieve success independently while developing intellectually, socially and cognitively through varied educational materials, activities and social interactions.
+
+Our L.E.A.P. curriculum is divided into 6 stages of development where children are involved in hands-on learning activities that encourage them to explore and solve problems at their own rate of development. Our L.E.A.P. curriculum includes sign language at the earliest stage and later adds phonics, manners, physical fitness, philanthropy and a foreign language (Spanish or Chinese-Mandarin).
+
+To learn more about these programs, please click on our Curriculum Wheel below.
+
+* at participating centers
+            </p>
             </div>
 
     )
--- little_angels_playhouse_client/src/components/Navbar.jsx
+++ little_angels_playhouse_client/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = (props) => {
     return (
 
        <ul>
-<li><a href="#" className="active">Home</a></li>
+<li><button onClick={props.toggleAbout } className="active">Home</button></li>
 <li><button onClick={props.toggleAbout } className="active">About</button></li>
 <li><button onClick={props.toggleExperience } className="active">Experience</button></li>
 <li><button onClick={props.toggleContact } className="active">Contact</button></li>
